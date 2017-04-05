76f7b7b1adf0adee264b374f4cab0ce6d8a1c607
DanielBar-On <danielbo@mellanox.com>
2017-04-05 16:50:04 +0300

    msg/async/rdma: add rdma local configuration for systemd
    issue: 992610
    
    The script is only called if rdma is found in ceph.conf.
    
    If ceph-conf or the script return an error, the comand/script is
    ignored and won't affect the service, since both ExecStartPre and
    ExecStopPost commands begin with the '-' sign.
    
    Service is only reloaded if a change was made to any of the conf files.
    
    No changes are kept after rdma is removed and services restarted (the
    script restores previous configuration when stopping the service).
    
    All changes are made in /run/systemd/system/ where all files are
    considered temporary and are  only kept for the current run of the service.
    
    Signed-off-by: DanielBar-On <danielbo@mellanox.com>
    Change-Id: I57d30cc4a3a828282cd65ac2df4c8302e063c0dc
    
    Conflicts:
    	systemd/ceph-disk@.service
    	systemd/ceph-mgr@.service

---
+++ src/systemd-rdma-settings.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+SERV=`echo $1 | awk -F@ '{print $1}'`
+FILE="/usr/lib/systemd/system/"$SERV"@.service"
+ARG_DIR="/run/systemd/system/"$1""
+NAME="$1"
+FLAG=false
+
+add_config(){
+	if [ -f "/run/systemd/system/ceph-osd.target.wants/"$NAME"" ]; then
+		sudo rm -rf "/run/systemd/system/ceph-osd.target.wants/"$NAME""
+	fi
+
+	for CONF in "$@"
+	do
+		if [ ! -f $CONF ]; then
+			touch $CONF
+			FLAG=true
+			cat $FILE > $CONF
+		fi
+		if ! grep -q "[Service]" $CONF ; then
+			echo "[Service]"| sudo tee -a $CONF
+			FLAG=true
+		fi
+		if grep -q "PrivateDevices=yes" $CONF; then
+			sudo sed -i 's/PrivateDevices=yes/PrivateDevices=no/g' $CONF
+			FLAG=true
+		fi
+		if ! grep -q "LimitMEMLOCK=infinity" $CONF ; then
+			sudo sed -i '/\[Service\]/a LimitMEMLOCK=infinity' $CONF
+			FLAG=true
+		fi
+	done
+}
+
+rm_config(){
+	if [ -f "/run/systemd/system/ceph-osd.target.wants/"$NAME"" ]; then
+		sudo rm -rf "/run/systemd/system/ceph-osd.target.wants/"$NAME""
+	fi
+
+	for CONF in "$@"
+	do
+		if [ -f $CONF ]; then
+			sudo rm -f $CONF
+			FLAG=true
+		fi
+	done
+}
+
+if echo $(ceph-conf ms_type) $(ceph-conf ms_public_type) $(ceph-conf ms_cluster_type) | egrep -q '*rdma*'; then
+	if [ "$2" != "clean" ]; then
+		add_config $MON_DIR $ARG_DIR $OSD_DIR
+		if [ "$FLAG" = true ]; then
+			sudo systemctl daemon-reload
+		fi
+	else
+		rm_config $MON_DIR $ARG_DIR $OSD_DIR
+		if [ "$FLAG" = true ]; then
+			sudo systemctl daemon-reload
+		fi
+	fi
+fi
